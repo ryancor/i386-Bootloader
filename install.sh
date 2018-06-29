1e2c4619e4aa1fe4e674f57648a47af0b142d1db
Ryan Cornateanu <ryan_cornateanu@yahoo.com>
2018-06-28 18:35:08 -0700

    adding ISO creation to install

--- install.sh
+++ install.sh
@@ -1,10 +1,21 @@
 nasm -f elf32 kernel.asm -o kasm.o
 echo "Finished compiling asm file"
 gcc -m32 -c kernel.c -o kc.o
-echo "Finished compiling user-land binary"
+echo "Finished compiling object files"
 ld -m elf_i386 -T link.ld -o image/kernel kasm.o kc.o
 echo "Completed linkage on both files.."
 rm -rf *.o
 
+echo "..."
+
+dd if=/dev/zero of=kernel.img bs=1024 count=1440
+dd if=image/kernel of=kernel.img seek=0 count=1 conv=notrunc
+echo "Finished creating image.."
+
+mv kernel.img image/
+genisoimage -quiet -V 'MYOS' -input-charset iso8859-1 -o image/kernel.iso -b kernel.img -hide kernel.img image/
+echo "Finished creating ISO....."
+
 echo "Starting emulation..."
 qemu-system-i386 -kernel image/kernel
+#qemu-system-i386 -cdrom image/kernel.iso
