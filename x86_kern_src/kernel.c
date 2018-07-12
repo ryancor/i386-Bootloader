74164e9905051c6f775cb3bf4c0d799375c52376
Ryan Cornateanu <ryan_cornateanu@yahoo.com>
2018-07-12 10:23:28 -0700

    exporting functions from assembly file

--- x86_kern_src/kernel.c
+++ x86_kern_src/kernel.c
@@ -1,6 +1,19 @@
 #include "headers/kernel.h"
 #include "headers/keyboard_map.h"
 
+// key map from header file
+extern unsigned char keyboard_map[128];
+
+// load defined functions from ASM file
+extern void key_handlr(void);
+extern char read_port(unsigned short port);
+extern void write_port(unsigned short port, unsigned char data);
+extern void load_idt(unsigned long *idt_ptr);
+
+// IDT_entry defined in header file
+typedef struct IDT_entry IDT[IDT_SIZE];
+
+
 void delay() {
 	for(unsigned int n = 0; n < 500000000; n++);
 }
@@ -36,7 +49,7 @@ void write_newline(void) {
 	current_loc = current_loc + (line_size - current_loc % (line_size));
 }
 
-void keyboard_handlr_main(void) {
+void key_handlr_main(void) {
 	return;
 }
 
