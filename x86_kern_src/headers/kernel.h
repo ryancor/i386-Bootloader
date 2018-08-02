bb0112da0e061349e978f602cf007a730a0e6a08
Ryan Cornateanu <ryan_cornateanu@yahoo.com>
2018-08-02 09:37:58 -0700

    align headers

--- x86_kern_src/headers/kernel.h
+++ x86_kern_src/headers/kernel.h
@@ -1,15 +1,15 @@
-#define LINES				25
+#define LINES 				25
+#define COLUMNS_IN_LINE 		80
 #define BYTES_FOR_EACH_ELEMENT 		2
-#define COLUMNS_IN_LINE			80
-#define SCREENSIZE			BYTES_FOR_EACH_ELEMENT * COLUMN_IN_LINE * LINES
+#define SCREENSIZE 			BYTES_FOR_EACH_ELEMENT * COLUMNS_IN_LINE * LINES
 
-#define KEYBOARD_DATA_PORT		0x60
-#define KEYBOARD_STATUS_PORT		0x64
-#define IDT_SIZE			256
-#define INTERRUPT_GATE			0x8e
-#define KERNEL_CODE_SEG_OFFSET		0x08
+#define KEYBOARD_DATA_PORT 		0x60
+#define KEYBOARD_STATUS_PORT 		0x64
+#define IDT_SIZE 			256
+#define INTERRUPT_GATE 			0x8e
+#define KERNEL_CODE_SEGMENT_OFFSET 	0x08
 
-#define ENTER_KEY_CODE			0x1c
+#define ENTER_KEY_CODE 			0x1C
 
 
 unsigned int current_loc = 0; // current cursor location
