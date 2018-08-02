ea395b2feb8c9c670df728073372ee9a77e1caec
Ryan Cornateanu <ryan_cornateanu@yahoo.com>
2018-08-02 09:41:26 -0700

    comment on interrupt gate

--- x86_kern_src/kernel.c
+++ x86_kern_src/kernel.c
@@ -64,7 +64,7 @@ void idt_init(void) {
 }
 
 void kb_init(void) {
-	/* 0xFD is 11111101 - enables only IRQ1 (keyboard)*/
+	// 0xFD is 11111101 - enables only IRQ1 (keyboard)
 	write_port(0x21, 0xFD);
 }
 
