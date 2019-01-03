6463170bcfc928f318cfa900110ba99139dddd6a
Ryan Cornateanu <ryan_cornateanu@yahoo.com>
2019-01-02 19:04:07 -0800

    framework for standard string handling

---
+++ x86_kern_src/grub_framework/headers/s_string.h
@@ -0,0 +1,17 @@
+#define size_t		unsigned long int
+
+void flush(char *var);
+
+void *memcpy(void *dest, const void *src, size_t n);
+
+void *memset(void *str, int c, size_t n);
+
+int strcmp(const char *stra, const char *strb);
+
+size_t strlen(const char *str);
+
+size_t strnlen(const char *s, size_t maxlen);
+
+char *strcat(char *dest, const char *src);
+
+char *s_strncpy(char *s1, const char *s2, size_t n);
---
+++ x86_kern_src/grub_framework/s_string.c
@@ -0,0 +1,100 @@
+#include "headers/s_string.h"
+
+void flush(char *var) 
+{
+	int i = 0;
+	while(var[i] != '\0') {
+		var[i] = '\0';
+		i++;
+	}
+}
+
+void *memcpy(void *dest, const void *src, size_t n)
+{
+	char *d = dest;
+	const char *s = src;
+	
+	for(size_t i = 0; i < n; i++)
+	{
+		d[i] = s[i];
+	}
+	return dest;
+}
+
+void *memset(void *str, int c, size_t n)
+{
+	char *s = str;
+
+	for(size_t i = 0; i < n; i++)
+	{
+		*(s + i) = c;
+	}
+
+	return str;
+}
+
+int strcmp(const char *stra, const char *strb)
+{
+	unsigned int i;
+
+	for(i = 0; stra[i] == strb[i]; i++)
+	{
+		if(stra[i] == '\0')
+		{
+			// string match
+			return 0;
+		}
+	}
+	return stra[i] - strb[i];
+}
+
+size_t strlen(const char *str)
+{
+	size_t length = 0;
+	while(str[length])
+	{
+		length++;
+	}
+	return length;
+}
+
+size_t strnlen(const char *s, size_t maxlen)
+{
+	size_t len;
+
+	for(len = 0; len < maxlen; len++, s++)
+	{
+		if(!*s)
+		{
+			break;
+		}
+	}
+	return (len);
+}
+
+char *strcat(char *dest, const char *src)
+{
+	unsigned int dest_size = strlen(dest);
+	unsigned int src_size = strlen(src);
+
+	char *d = dest;
+
+	for(size_t i = 0; i < src_size; i++)
+	{
+		d[dest_size + i] = src[i];
+	}
+
+	d[dest_size + src_size] = '\0';
+
+	return dest;
+}
+
+char *s_strncpy(char *s1, const char *s2, size_t n)
+{
+	size_t size = strnlen(s2, n);
+	if(size != n)
+	{
+		memset(s1 + size, '\0', n - size);
+	}	
+	return memcpy(s1, s2, size);
+}
