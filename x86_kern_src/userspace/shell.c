c881245054e86425b1d79778cddb395a671e67d0
Ryan Cornateanu <ryan_cornateanu@yahoo.com>
2019-01-15 10:03:51 -0800

    user area to access shell

---
+++ x86_kern_src/userspace/shell.c
@@ -0,0 +1,42 @@
+#include "../headers/ports.h"
+#include "../headers/kernel.h"
+#include "../headers/screen.h"
+#include "../headers/shell.h"
+#include "../grub_framework/headers/s_string.h"
+
+void terminal_commands(char *keycode_string)
+{
+	if(strcmp(keycode_string, "ls") == 0)
+	{
+		kprint("/home    /var    /lib", 0x07);
+		kprint_newline();
+	}
+	else if(strcmp(keycode_string, "pwd") == 0) {
+		kprint("/", 0x07);
+		kprint_newline();
+	}
+	else if(strcmp(keycode_string, "clear") == 0) {
+		clear_screen();
+	}
+	else if(strcmp(keycode_string, "restart") == 0) {
+		kprint("Resetting...", 0x07);
+		delay();
+		APMInterface_Init();
+	}
+	else if(strcmp(keycode_string, "shutdown") == 0) {
+		kprint("Goodbye!", 0x07);
+		// disable interrupts, then halt the CPU
+		__asm__("cli; \
+			hlt");
+	}
+	else if(strcmp(keycode_string, "whoami") == 0) {
+		kprint("Ringo, but lets call you root ;)", 0x07);
+		kprint_newline();
+	}
+	else {
+		kprint("Command ", 0x07);
+		kprint(keycode_string, 0x08);
+		kprint(" not recognized.", 0x07);
+		kprint_newline();
+	}
+}
