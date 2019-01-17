cba8418b724d943fcad09a2e583758edc828ce00
Ryan Cornateanu <ryan_cornateanu@yahoo.com>
2019-01-16 17:36:10 -0800

    headers for boolean values and unsigned integers

---
+++ x86_kern_src/grub_framework/headers/s_stdint.h
@@ -0,0 +1,21 @@
+#ifndef __int8_t_defined
+#define __int8_t_defined
+typedef signed char                	int8_t;
+typedef short int                	int16_t;
+typedef int                        	int32_t;
+#if __WORDSIZE == 64
+typedef long int                	int64_t;
+#else
+typedef long long int                	int64_t;
+#endif
+#endif
+
+/* Unsigned.  */
+typedef unsigned char                	uint8_t;
+typedef unsigned short int        	uint16_t;
+typedef unsigned int                	uint32_t;
+#if __WORDSIZE == 64
+typedef unsigned long int        	uint64_t;
+#else
+typedef unsigned long long int        	uint64_t;
+#endif
