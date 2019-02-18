2bcef185754f40415b9e401e4f0ca8570ee21270
Ryan Cornateanu <ryan_cornateanu@yahoo.com>
2019-02-18 11:14:50 -0800

    adding port byte out to the installer of the setting IDT

--- x86_kern_src/cpu/isr.c
+++ x86_kern_src/cpu/isr.c
@@ -41,6 +41,36 @@ void isr_install()
 	set_idt_gate(30, (u32)isr30);
 	set_idt_gate(31, (u32)isr31);
 
+	// Remap the PIC
+	port_byte_out(0x20, 0x11);
+	port_byte_out(0xA0, 0x11);
+	port_byte_out(0x21, 0x20);
+	port_byte_out(0xA1, 0x28);
+	port_byte_out(0x21, 0x04);
+	port_byte_out(0xA1, 0x02);
+	port_byte_out(0x21, 0x01);
+	port_byte_out(0xA1, 0x01);
+	port_byte_out(0x21, 0x0);
+	port_byte_out(0xA1, 0x0);
+
+	// Install the IRQs
+	set_idt_gate(32, (u32)irq0);
+	set_idt_gate(33, (u32)irq1);
+	set_idt_gate(34, (u32)irq2);
+	set_idt_gate(35, (u32)irq3);
+	set_idt_gate(36, (u32)irq4);
+	set_idt_gate(37, (u32)irq5);
+	set_idt_gate(38, (u32)irq6);
+	set_idt_gate(39, (u32)irq7);
+	set_idt_gate(40, (u32)irq8);
+	set_idt_gate(41, (u32)irq9);
+	set_idt_gate(42, (u32)irq10);
+	set_idt_gate(43, (u32)irq11);
+	set_idt_gate(44, (u32)irq12);
+	set_idt_gate(45, (u32)irq13);
+	set_idt_gate(46, (u32)irq14);
+	set_idt_gate(47, (u32)irq15);
+
 	set_idt();
 }
 
