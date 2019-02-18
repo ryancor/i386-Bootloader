e7cadb6dca1608b4bb6a1c6face9a3d5a3c984a2
Ryan Cornateanu <ryan_cornateanu@yahoo.com>
2019-02-18 11:07:23 -0800

    irq_handler to be sent after every interrupt

--- x86_kern_src/cpu/isr.c
+++ x86_kern_src/cpu/isr.c
@@ -1,7 +1,10 @@
 #include "isr.h"
 #include "idt.h"
+#include "../headers/ports.h"
 #include "../headers/screen.h"
 #include "../grub_framework/headers/s_string.h"
+
+isr_t interrupt_handlers[256];
 
 void isr_install()
 {
@@ -90,4 +93,28 @@ void isr_handler(registers_t r)
 	kprint("\n", 0x07);
 	kprint(exception_messages[r.int_no], 0x08);
 	kprint("\n", 0x07);
+}
+
+void register_interrupt_handler(u8 n, isr_t handler)
+{
+	interrupt_handlers[n] = handler;
+}
+
+void irq_handler(registers_t r)
+{
+	// After every interrupt we need to send an EOI to the PICs
+	// or they will not send another interrupt again
+	if(r.int_no >= 40)
+	{
+		port_byte_out(0xA0, 0x20); // slave
+	}
+
+	port_byte_out(0x20, 0x20); // master
+
+	// Handle the interrupt in a modular way
+	if(interrupt_handlers[r.int_no] != 0)
+	{
+		isr_t handler = interrupt_handlers[r.int_no];
+		handler(r);
+	}
 }
