777ba8eb780ef631c9b7602f69d964acdc79bca8
Ryan Cornateanu <ryan_cornateanu@yahoo.com>
2019-03-18 15:36:49 -0700

    compile bootloader from boot directory

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-nasm -f elf32 x86_kern_src/kernel/kernel.asm -o x86_kern_src/kernel/kasm.o
+nasm -f elf32 x86_kern_src/boot/kernel_entry.asm -o x86_kern_src/boot/kasm.o
 nasm -f elf32 x86_kern_src/cpu/interrupt.asm -o x86_kern_src/cpu/interrupt.o
 echo "Finished compiling asm file"
 gcc -fno-stack-protector -m32 -c x86_kern_src/kernel/kernel.c -o x86_kern_src/kernel/kc.o
@@ -16,10 +16,11 @@ gcc -fno-stack-protector -m32 -c x86_kern_src/kernel/fs/fs.c -o x86_kern_src/ker
 gcc -fno-stack-protector -m32 -c x86_kern_src/kernel/fs/list.c -o x86_kern_src/kernel/fs/list.o
 gcc -fno-stack-protector -m32 -c x86_kern_src/kernel/fs/pipe.c -o x86_kern_src/kernel/fs/pipe.o
 echo "Finished compiling object files"
-ld -m elf_i386 -T link/link.ld -o image/kernel x86_kern_src/kernel/kasm.o x86_kern_src/kernel/kc.o x86_kern_src/cpu/interrupt.o x86_kern_src/cpu/idt.o x86_kern_src/cpu/isr.o x86_kern_src/cpu/timer.o x86_kern_src/userspace/shell.o x86_kern_src/grub_framework/s_string.o x86_kern_src/kernel/ports/ports.o x86_kern_src/kernel/fs/fs.o x86_kern_src/kernel/fs/pipe.o x86_kern_src/kernel/fs/list.o x86_kern_src/drivers/screen.o x86_kern_src/drivers/mouse.o x86_kern_src/drivers/cmos.o x86_kern_src/kernel/interrupts/int.o
+ld -m elf_i386 -T link/link.ld -o image/kernel x86_kern_src/boot/kasm.o x86_kern_src/kernel/kc.o x86_kern_src/cpu/interrupt.o x86_kern_src/cpu/idt.o x86_kern_src/cpu/isr.o x86_kern_src/cpu/timer.o x86_kern_src/userspace/shell.o x86_kern_src/grub_framework/s_string.o x86_kern_src/kernel/ports/ports.o x86_kern_src/kernel/fs/fs.o x86_kern_src/kernel/fs/pipe.o x86_kern_src/kernel/fs/list.o x86_kern_src/drivers/screen.o x86_kern_src/drivers/mouse.o x86_kern_src/drivers/cmos.o x86_kern_src/kernel/interrupts/int.o
 echo "Completed linkage on both files.."
 rm -rf x86_kern_src/*.o
 rm -rf x86_kern_src/cpu/*.o
+rm -rf x86_kern_src/boot/*.o
 rm -rf x86_kern_src/kernel/*.o
 rm -rf x86_kern_src/userspace/*.o
 rm -rf x86_kern_src/grub_framework/*.o
