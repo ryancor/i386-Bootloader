eb4815e7ecf65c5e1eb03c50affcd85a0ff31c4d
Ryan Cornateanu <ryan_cornateanu@yahoo.com>
2019-03-18 15:38:24 -0700

    function definitions for port reading

--- x86_kern_src/kernel/ports/ports.c
+++ x86_kern_src/kernel/ports/ports.c
@@ -1,3 +1,4 @@
+#include "../../headers/ports.h"
 #include "../../headers/screen.h"
 
 unsigned char port_byte_in(unsigned short port)
@@ -11,6 +12,18 @@ unsigned char port_byte_in(unsigned short port)
 void port_byte_out(unsigned int port, unsigned char data)
 {
 	__asm__("out %%al, %%dx" : : "a" (data), "d" (port));
+}
+
+unsigned short port_word_in(uint16_t port)
+{
+	uint16_t result;
+	asm("in %%dx, %%ax" : "=a" (result) : "d" (port));
+	return result;
+}
+
+void port_word_out(uint16_t port, uint16_t data)
+{
+	asm volatile("out %%ax, %%dx" : : "a" (data), "d" (port));
 }
 
 void APMInterface_Init()
