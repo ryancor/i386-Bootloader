44704458de2dbfe3cebcf296e8700da813a0ff39
Ryan Cornateanu <ryan_cornateanu@yahoo.com>
2019-03-18 15:37:50 -0700

    API handlers for port_word in & out

--- x86_kern_src/headers/ports.h
+++ x86_kern_src/headers/ports.h
@@ -1,3 +1,7 @@
+#include "../grub_framework/headers/s_stdint.h"
+
 unsigned char port_byte_in(unsigned short port);
 void port_byte_out(unsigned int port, unsigned char data);
+unsigned short port_word_in(uint16_t port);
+void port_word_out(uint16_t port, uint16_t data);
 void APMInterface_Init();
