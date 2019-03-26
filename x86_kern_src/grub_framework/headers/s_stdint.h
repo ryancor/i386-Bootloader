fa10fa25b23031604448b4a52b4a4a4b328202ad
Ryan Cornateanu <ryan_cornateanu@yahoo.com>
2019-03-25 17:07:28 -0700

    define unsigned long to uintptr_t

--- x86_kern_src/grub_framework/headers/s_stdint.h
+++ x86_kern_src/grub_framework/headers/s_stdint.h
@@ -1,3 +1,4 @@
+#define NULL				((void*)0)
 #ifndef __int8_t_defined
 #define __int8_t_defined
 typedef signed char                	int8_t;
@@ -19,3 +20,4 @@ typedef unsigned long int        	uint64_t;
 #else
 typedef unsigned long long int        	uint64_t;
 #endif
+typedef unsigned long                   uintptr_t;
