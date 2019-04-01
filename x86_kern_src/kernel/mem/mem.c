7871c47f8699511241c4302d26b9dda21831caa1
Ryan Cornateanu <ryan_cornateanu@yahoo.com>
2019-04-01 15:40:35 -0700

    sched tasks based on page allocation

--- x86_kern_src/kernel/mem/mem.c
+++ x86_kern_src/kernel/mem/mem.c
@@ -9,6 +9,7 @@ uint32_t *frames;
 uint32_t nframes;
 uintptr_t placement_pointer = (uintptr_t)&end;
 uintptr_t heap_end = (uintptr_t)NULL;
+page_directory_t *current_directory;
 page_directory_t *kernel_directory;
 
 void paging_install(uint32_t memsize)
@@ -37,3 +38,17 @@ void heap_install(void)
 {
 	heap_end = (placement_pointer + 0x1000) & ~0xFFFF;
 }
+
+void switch_page_directory(page_directory_t *dir)
+{
+	current_directory = dir;
+	asm volatile(
+		"mov %0, %%cr3\n"
+		"mov %%cr0, %%eax\n"
+		"orl $0x80000000, %%eax\n"
+		"mov %%eax, %%cr3\n"
+		:: "r"(dir->physical_address)
+		: "%eax"
+	);
+}
+
